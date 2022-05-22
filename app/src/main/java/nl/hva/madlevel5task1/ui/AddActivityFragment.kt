80ca06a0cd97637fc195a0214eb6750fa609df2a
Kevin Breurken <kevinbreurken@gmail.com>
2022-05-23 01:10:18 +0200

    Add level1 as baseproject

---
+++ app/src/main/java/nl/hva/madlevel5task1/ui/AddActivityFragment.kt
@@ -0,0 +1,44 @@
+package nl.hva.madlevel5task1.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.example.madlevel5task1.databinding.FragmentAddActivityBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class AddActivityFragment : Fragment() {
+
+    private var _binding: FragmentAddActivityBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentAddActivityBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/nl/hva/madlevel5task1/ui/BacklogFragment.kt
@@ -0,0 +1,43 @@
+package nl.hva.madlevel5task1.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.madlevel5task1.R
+import com.example.madlevel5task1.databinding.FragmentBacklogBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class BacklogFragment : Fragment() {
+
+    private var _binding: FragmentBacklogBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentBacklogBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
