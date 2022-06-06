55212146d14057622f39119ae334d17675fb7162
Kevin Breurken <kevinbreurken@gmail.com>
2022-06-07 01:38:01 +0200

    Delete all button added

--- app/src/main/java/nl/hva/madlevel5task1/ui/MainActivity.kt
+++ app/src/main/java/nl/hva/madlevel5task1/ui/MainActivity.kt
@@ -4,30 +4,56 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.madlevel5task1.R
 import com.example.madlevel5task1.databinding.ActivityMainBinding
+import nl.hva.madlevel5task1.vm.GameViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: GameViewModel by viewModels()
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setSupportActionBar(binding.toolbar)
+
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-
-        navController = navHostFragment.navController
-
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
         fabToggler()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.deleteAction) {
+            viewModel.deleteGames()
+        }
+
+        return when (item.itemId) {
+            R.id.deleteAction -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 
     private fun fabToggler() {
@@ -39,5 +65,4 @@ class MainActivity : AppCompatActivity() {
 //            }
 //        }
     }
-
 }
